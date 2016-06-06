fc637114397585fda39e8d66448eb45671c5c842
Tom Faulhaber <git_com@infolace.com>
2016-06-06 11:04:56 -0700

    Autodoc commit for master/5a12963a

--- index-0.1.0.clj
+++ index-0.1.0.clj
@@ -3,7 +3,7 @@
    :author "Jeffrey Straszheim",
    :name "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph/clojure.algo.graph-api.html",
+   "http://clojure.github.io/algo.graph/clojure.algo.graph-api.html",
    :source-url
    "https://github.com/clojure/algo.graph/blob/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj"}),
  :vars
@@ -19,7 +19,7 @@
    :doc "For each node n, add the edge n->n if not already present.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/add-loops"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/add-loops"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "component-graph",
@@ -33,7 +33,7 @@
    "Given a graph, perhaps with cycles, return a reduced graph that is acyclic.\nEach node in the new graph will be a set of nodes from the old.\nThese sets are the strongly connected components.  Each edge will\nbe the union of the corresponding edges of the prior graph.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/component-graph"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/component-graph"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "dependency-list",
@@ -47,7 +47,7 @@
    "Similar to a topological sort, this returns a vector of sets. The\nset of nodes at index 0 are independent.  The set at index 1 depend\non index 0; those at 2 depend on 0 and 1, and so on.  Those withing\na set have no mutual dependencies.  Assume the input graph (which\nmuch be acyclic) has an edge a->b when a depends on b.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/dependency-list"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/dependency-list"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "fixed-point",
@@ -61,7 +61,7 @@
    "Repeatedly apply fun to data until (equal old-data new-data)\nreturns true.  If max iterations occur, it will throw an\nexception.  Set max to nil for unlimited iterations.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/fixed-point"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/fixed-point"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "get-neighbors",
@@ -74,7 +74,7 @@
    :doc "Get the neighbors of a node.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/get-neighbors"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/get-neighbors"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "lazy-walk",
@@ -88,7 +88,7 @@
    "Return a lazy sequence of the nodes of a graph starting a node n.  Optionally,\nprovide a set of visited notes (v) and a collection of nodes to\nvisit (ns).",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/lazy-walk"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/lazy-walk"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "post-ordered-nodes",
@@ -102,7 +102,7 @@
    "Return a sequence of indexes of a post-ordered walk of the graph.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/post-ordered-nodes"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/post-ordered-nodes"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "recursive-component?",
@@ -115,7 +115,7 @@
    :doc "Is the component (recieved from scc) self recursive?",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/recursive-component?"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/recursive-component?"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "remove-loops",
@@ -128,7 +128,7 @@
    :doc "For each node n, remove any edges n->n.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/remove-loops"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/remove-loops"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "reverse-graph",
@@ -142,7 +142,7 @@
    "Given a directed graph, return another directed graph with the\norder of the edges reversed.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/reverse-graph"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/reverse-graph"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "scc",
@@ -156,7 +156,7 @@
    "Returns, as a sequence of sets, the strongly connected components\nof g.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/scc"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/scc"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "self-recursive-sets",
@@ -170,7 +170,7 @@
    "Returns, as a sequence of sets, the components of a graph that are\nself-recursive.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/self-recursive-sets"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/self-recursive-sets"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "stratification-list",
@@ -184,7 +184,7 @@
    "Similar to dependency-list (see doc), except two graphs are\nprovided.  The first is as dependency-list.  The second (which may\nhave cycles) provides a partial-dependency relation.  If node a\ndepends on node b (meaning an edge a->b exists) in the second\ngraph, node a must be equal or later in the sequence.",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/stratification-list"}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/stratification-list"}
   {:raw-source-url
    "https://github.com/clojure/algo.graph/raw/03c740c61aecb55225bdad19813e316f44837142/src/main/clojure/clojure/algo/graph.clj",
    :name "transitive-closure",
@@ -198,4 +198,4 @@
    "Returns the transitive closure of a graph.  The neighbors are lazily computed.\n\nNote: some version of this algorithm return all edges a->a\nregardless of whether such loops exist in the original graph.  This\nversion does not.  Loops will be included only if produced by\ncycles in the graph.  If you have code that depends on such\nbehavior, call (-> g transitive-closure add-loops)",
    :namespace "clojure.algo.graph",
    :wiki-url
-   "http://clojure.github.com/algo.graph//clojure.algo.graph-api.html#clojure.algo.graph/transitive-closure"})}
+   "http://clojure.github.io/algo.graph//index.html#clojure.algo.graph/transitive-closure"})}
